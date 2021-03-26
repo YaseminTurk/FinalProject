2dae1d1d2cc57bad03bbde86ff92922a77f4bf00
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-27 02:52:36 +0300

    8.gün 1.commit

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class = referans tip
+    //IEntity = IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() = new'lenebilir bir nesne olmalı (IEntity i devre dışı bırakmak için)
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); //filtre verilmezse tüm datayı getirir.
+        T Get(Expression<Func<T, bool>> filter); //filtre zorunlu
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
