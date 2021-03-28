92e35570357ebabfb8412db3793a10a9017634ff
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-28 15:03:39 +0300

    8.gün 2.commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,18 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //S'O'LID  = open closed principle  // sisteme yeni bir özellik ekliyorsak mevcuttaki hiçbir koda dokunamazsın.
+
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
             }
