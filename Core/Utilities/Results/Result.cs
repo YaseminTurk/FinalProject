60f0df5f25a14c0ed8e6f6d469918de095f6e692
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-30 00:04:30 +0300

    10.gün

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+
+        public Result(bool success, string message) :this(success) //resultun tek parametreli constructor'una successi gönderir. //burası iki contsructorı da çalıştırır.
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success; //success i set etme işini buraya yazıyoruz çünkü burada mecburuz.
+        }
+
+        public bool Success { get; } //readonly ler constructer içinde set edilebilirler.
+
+        public string Message { get;  }
+    }
+}
