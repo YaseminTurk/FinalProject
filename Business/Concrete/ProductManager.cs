0b97651327ff567c7e88cf98e4da709cb6a6d7f5
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-04-01 22:56:02 +0300

    12.gün 1.commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,15 +23,10 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if (product.UnitPrice<=0)
-            {
-                return new ErrorResult(Messages.UnitPriceInvalid);
-            }
+            
 
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            ValidationTool.Validate(new ProductValidator() ,product);
+
             _productDal.Add(product);
 
             return new Result(true,Messages.ProductAdded);
